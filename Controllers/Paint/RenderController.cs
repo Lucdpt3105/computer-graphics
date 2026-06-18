5a3a3f3515c2d9a898a08d064201db9040ce809a
game-dev-newbie <bdat6832@gmail.com>
2026-06-18 18:38:24 +0700

    update structure

--- Controllers/Paint/RenderController.cs
+++ Controllers/Paint/RenderController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project_CG_Paint.Controllers.Paint
 {
-    internal class RenderController
+    public class RenderController
     {
     }
 }
--- Controllers/Paint/SelectionController.cs
+++ Controllers/Paint/SelectionController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project_CG_Paint.Controllers.Paint
 {
-    internal class SelectionController
+    public class SelectionController
     {
     }
 }
